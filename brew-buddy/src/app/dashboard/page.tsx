bb8c6ad4ae8e48c77aa2a3736a5cfca53dea629e
zaknolte <127704632+zaknolte@users.noreply.github.com>
2024-08-08 20:17:10 -0700

    initial dashboard page

---
+++ brew-buddy/src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { useState } from "react"
+import { useRouter } from "next/navigation"
+
+export default function DashboardPage() {
+    return (
+        <p className="mt-10">
+            test
+        </p>
+    )
+}
