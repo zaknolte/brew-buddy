2208bcc00dd45c2d35def9f2002b0565ef244cb7
zaknolte <127704632+zaknolte@users.noreply.github.com>
2024-08-08 20:16:07 -0700

    add signin route for middleware

--- brew-buddy/src/app/api/auth/[...nextauth]/route.ts
+++ brew-buddy/src/app/api/auth/[...nextauth]/route.ts
@@ -50,6 +50,9 @@ export const authOptions: NextAuthOptions = {
     },
     secret: process.env.NEXTAUTH_SECRET,
     debug: process.env.NODE_ENV === "development",
+    pages: {
+        signIn: '/login'
+    }
 };
 
 const handler = NextAuth(authOptions);
